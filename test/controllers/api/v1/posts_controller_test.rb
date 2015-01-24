0547781bc8adf9121339535f070efb1d1aab8653
flowerwrong <sysuyangkang@gmail.com>
2015-01-24 18:07:08 +0800

    un solve the problem NameError: uninitialized constant Api in test.

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -1,3 +1,9 @@
 class Api::V1::PostsControllerTest < ActionController::TestCase
   self.use_instantiated_fixtures = true
+
+  test "should get post index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:posts)
+  end
 end
