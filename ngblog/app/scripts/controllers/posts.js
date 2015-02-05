1b87570e9a9712850703d355fb8166220a07f2c5
gang <499801006@qq.com>
2015-02-05 11:07:37 +0800

    fixed posts.html pagination bug
    修复了文章列表页面的分页bug

--- ngblog/app/scripts/controllers/posts.js
+++ ngblog/app/scripts/controllers/posts.js
@@ -10,27 +10,30 @@
 angular.module('ngblogApp')
   .controller('PostsCtrl', ['$scope', '$log', 'postsService', '$location', '$anchorScroll', function($scope, $log, postsService,$location,$anchorScroll) {
     $scope.posts = [];
-    postsService.getPosts(1, 10, 0).
+    $scope.itemsPerPage = 5;
+    $scope.bigTotalItems = null;
+    $scope.bigCurrentPage = 1;
+    $scope.maxSize = 8;
+    $location.hash('top');
+    postsService.getPosts($scope.bigCurrentPage, $scope.itemsPerPage, 0).
       success(function(data, status, headers, config) {
         $scope.posts = data.data.posts;
-        $scope.totalItems = data.data.total_count;
+        $scope.bigTotalItems = data.data.total_count;
       }).
       error(function(data, status, headers, config) {
-        console.log(data);
       });
 
     $scope.pageChanged = function() {
-      postsService.getPosts($scope.currentPage, 10, 0).
+      postsService.getPosts($scope.bigCurrentPage, $scope.itemsPerPage, 0).
       success(function(data, status, headers, config) {
         $scope.posts = data.data.posts;
+        $scope.bigTotalItems = data.data.total_count;
       }).
       error(function(data, status, headers, config) {
-        console.log(data);
       });
       $log.log('Page changed to: ' + $scope.currentPage);
       };
     $scope.scrollTop = function() {
-      $location.hash('top');
       $anchorScroll();
     }
   }]);
