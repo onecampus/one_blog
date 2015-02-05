514d158a2029d5a2fad875b010bfc812fd9b33c8
flowerwrong <sysuyangkang@gmail.com>
2015-02-05 17:01:30 +0800

    Fix login page and login logical.

--- ngblog/app/scripts/controllers/login.js
+++ ngblog/app/scripts/controllers/login.js
@@ -8,6 +8,27 @@
  * Controller of the ngblogApp
  */
 angular.module('ngblogApp')
-  .controller('LoginCtrl', ['$scope', function ($scope) {
-    $scope.username = "hehe";
+  .controller('LoginCtrl', ['$scope', 'AuthService', function ($scope, AuthService) {
+    $scope.user = {
+      email: '',
+      password: '',
+    };
+    $scope.login = function() {
+      var _loginUser = {
+        login: $scope.user
+      };
+      console.log(_loginUser);
+      AuthService.login(_loginUser).
+        success(function(data, status, headers, config) {
+          console.log(data);
+          if(data.auth_token) {
+            alert('登陆成功');
+          } else if(data.error) {
+            alert('用户名或者密码错误, 请重新输入');
+          }
+        }).
+        error(function(data, status, headers, config) {
+          console.log(data);
+        });
+    };
   }]);
