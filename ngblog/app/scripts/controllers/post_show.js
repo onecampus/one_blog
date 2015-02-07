677b481e44d15e80103517edc32f197a3ae87e55
gang <499801006@qq.com>
2015-02-07 15:23:35 +0800

    fixed nav.html style bug
    修复了后台导航栏的样式ｂｕｇ

--- ngblog/app/scripts/controllers/post_show.js
+++ ngblog/app/scripts/controllers/post_show.js
@@ -10,14 +10,22 @@
 angular.module('ngblogApp')
   .controller('PostShowCtrl', ['$scope', '$log', 'postsService', '$routeParams', '$controller', function($scope, $log, postsService, $routeParams, $controller) {
     $scope.post = null;
+    $scope.postimg = null;
+    $scope.imageMark = false;
     postsService.getPostById($routeParams.postId).
       success(function(data) {
         $scope.post = data.post;
+        $scope.postimg = data.post.img;
+        if($scope.postimg === null || $scope.postimg === "") {
+          $scope.imageMark = false;
+        }
+        else {
+          $scope.imageMark = true;
+        }
       }).
       error(function(data) {
         console.log(data);
       });
-
     $scope.posts = [];
     postsService.getPosts(1, 14, 0).
       /* jshint camelcase: false */
