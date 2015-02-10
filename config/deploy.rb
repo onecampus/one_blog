ba0ecbbeeaa47de7cc80e47e800db8c015237641
flowerwrong <sysuyangkang@gmail.com>
2015-02-10 10:39:58 +0800

    Change the ip for deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:onecampus/one_blog.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/ubuntu/www/one_blog'
+set :deploy_to, '/home/flowerwrong/www/one_blog'
 
 # Default value for :scm is :git
 set :scm, :git
